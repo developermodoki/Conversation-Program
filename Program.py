410088c09e5946f443a8c3d55bcb9c72e76ea2bd
Windows1000000 <61893884+developermodoki@users.noreply.github.com>
2020-03-27 16:59:20 +0900

    Program.py

---
+++ Program.py
@@ -0,0 +1,15 @@
+answer = input("今日はいい天気ですね。はい。= 1、いいえ。 = 2 :")
+int_answer = int(answer)
+if int_answer == 1 :
+    print("そうですよねー。")
+
+if int_answer == 2 :
+    print("そうですか...残念です")
+
+answer2 = input("今日の気分はどうですか？ けっこういいです = 1、良くないです = 2 :")
+int_answer2 = int(answer2)
+if int_answer2 == 1 :
+    print("そうですか。最高の一日にしてください。")
+if int_answer2 == 2 :
+    print("そうですか...残念です。この一日でリフレッシュできるように頑張ってください。")
+
